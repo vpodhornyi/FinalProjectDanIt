ca27156b5388ebffcf9f4a4aa8128a94ccbc09a9
Vadym Tartakovskyi <62440835+CodeVaDOs@users.noreply.github.com>
2022-01-11 16:30:31 +0200

    Feature/client (#2)
    * init
    
    * add client .gitignore
    
    * update .gitignore
    
    * cleaned
    
    * auth page finish
    
    * delete antd & add reset css
    
    * add common dialog window
    
    * add close button to dialog window
    
    * add sing up first step base
    
    * second step sing up base & response dialog window
    
    * add select to text field
    
    * sing in first step
    
    * sing in second step & forgot password
    
    * backend structure
    
    Co-authored-by: viktor <vpodhornyi@gmail.com>

---
+++ src/test/java/com/twitter/fs14/Fs14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.fs14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fs14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
